9fd0e70d12435f9b0fb2c3c4c0f840c858853744
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-07 07:37:08 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FGTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double salario;
+
+            Console.Write("Digite seu Salário (R$)..: ");
+            salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            double salario1;
+            double salariores;
+
+            salario1 = (8 * salario);
+            salariores = salario1 / 100;
+
+            Console.Write($"FGTS: R$ {salariores}");
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione qualquer tecla para Finalizar...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
